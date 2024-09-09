fe48ede3fc2affe63a9f3126667909b9625d1adb
Alexandre <alexandre@endor.ai>
2024-09-09 10:37:44 -1000

    hehe

--- src/main/java/com/acme/foo/MainController.java
+++ src/main/java/com/acme/foo/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.impl.CreatorCollector;
 
 @Configuration
 @ComponentScan({"com.acme.foo"})
@@ -35,7 +36,11 @@ public class MainController extends SpringBootServletInitializer {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
-	}
+
+        CreatorCollector coll = new CreatorCollector(null, null);
+        coll.addDelegatingCreator(null, true, null);
+
+	}   
 	
 	@Bean
     public ObjectMapper objectMapper() {
